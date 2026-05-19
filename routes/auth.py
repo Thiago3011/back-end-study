317648eae98a3949e21024796336e0d71f74c07b
Thiago3011 <thiago.silva1001@outlook.com>
2026-05-19 19:28:45 -0300

    feat: implement protected route with JWT authentication

--- routes/auth.py
+++ routes/auth.py
@@ -1,6 +1,6 @@
-from fastapi import APIRouter
+from fastapi import APIRouter, Depends
 from models.login_request import LoginRequest
-from services import auth_services
+from services import auth_services, auth_handler
 
 router = APIRouter(
     prefix="/auth",
@@ -10,3 +10,7 @@ router = APIRouter(
 @router.post("/login")
 def login(login_data: LoginRequest):
     return auth_services.get_user_by_email(login_data)
+
+@router.get("/me")
+def get_me(current_user = Depends(auth_handler.get_current_user_with_token)):
+    return current_user
--- services/auth_handler.py
+++ services/auth_handler.py
@@ -1,10 +1,23 @@
 from jose import jwt
+from fastapi.security import OAuth2PasswordBearer
+from database import users
+from fastapi import HTTPException, Depends
 
 SECRET_KEY = "chave_secreta"
 ALGORITHM = "HS256"
+
+oauth2_scheme = OAuth2PasswordBearer(tokenUrl="auth/login")
 
 def create_access_token(user_id):
     return jwt.encode({"user_id": user_id}, key=SECRET_KEY, algorithm=ALGORITHM)
 
 def decode_access_token(token):
     return jwt.decode(token, key=SECRET_KEY, algorithms=[ALGORITHM])
+
+def get_current_user_with_token(token: str = Depends(oauth2_scheme)):
+    token_decoded = decode_access_token(token)
+    user_id_decoded = token_decoded["user_id"]
+    for user in users:
+        if user.id == user_id_decoded:
+            return {"id": user.id, "email": user.email}
+    raise HTTPException(detail="User is not with a valid token", status_code=401)
